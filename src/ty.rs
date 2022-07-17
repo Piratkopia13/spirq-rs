2d3debc90ceb655856a8ce3964fc1e58ea4b23b7
Alexander Wester <piratkopia13@gmail.com>
2022-07-17 15:43:57 +0200

    Temp fix for dxc generated spirv from hlsl returns is_depth=2, until this is fixed in upstream repo

--- src/ty.rs
+++ src/ty.rs
@@ -152,8 +152,10 @@ impl ImageUnitFormat {
     pub fn from_spv_def(is_sampled: u32, is_depth: u32, color_fmt: ImageFormat) -> Result<ImageUnitFormat> {
         let img_unit_fmt = match (is_sampled, is_depth, color_fmt) {
             (1, 0, _) => ImageUnitFormat::Sampled,
+            (1, 2, _) => ImageUnitFormat::Sampled,
             (1, 1, _) => ImageUnitFormat::Depth,
             (2, 0, color_fmt) => ImageUnitFormat::Color(color_fmt),
+            (2, 2, color_fmt) => ImageUnitFormat::Color(color_fmt),
             _ => return Err(Error::UNSUPPORTED_IMG_CFG),
         };
         Ok(img_unit_fmt)
